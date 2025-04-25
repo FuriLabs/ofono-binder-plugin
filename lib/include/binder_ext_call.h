3177bae2719aae436b03ff024785386e898dbe33
Bardia Moshiri <bardia@furilabs.com>
2025-04-25 11:27:15 -0400

    Revert "[ofono-binder-plugin] Add support for external ringback tone. JB#58545"
    This reverts commit 7804af7dfd776ddf40d771cfa6ceebd2783cfb8c.

--- lib/include/binder_ext_call.h
+++ lib/include/binder_ext_call.h
@@ -161,13 +161,6 @@ void
     BINDER_EXT_CALL_DISCONNECT_REASON reason,
     void* user_data);
 
-typedef
-void
-(*BinderExtCallRingbackToneFunc)(
-    BinderExtCall* ext,
-    gboolean start,
-    void* user_data);
-
 GType binder_ext_call_get_type(void);
 #define BINDER_EXT_TYPE_CALL (binder_ext_call_get_type())
 #define BINDER_EXT_CALL(obj) (G_TYPE_CHECK_INSTANCE_CAST(obj, \
@@ -294,12 +287,6 @@ void
 binder_ext_call_remove_handler(
     BinderExtCall* ext,
     gulong id);
-
-gulong
-binder_ext_call_add_ringback_tone_handler(
-    BinderExtCall* ext,
-    BinderExtCallRingbackToneFunc handler,
-    void* user_data); /* Since 1.1.22 */
 
 void
 binder_ext_call_remove_handlers(
